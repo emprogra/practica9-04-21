00837d7f58eb408f453d37877281463a42d091d9
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-09 21:14:01 -0400

    Creando modelo

--- practica/practica1.9-04/Models/Person.cs
+++ practica/practica1.9-04/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace practica1._9_04.Models
 {
     public class Person
     {
+        [Key]
+        public int Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Direccion { get; set; }
     }
 }
